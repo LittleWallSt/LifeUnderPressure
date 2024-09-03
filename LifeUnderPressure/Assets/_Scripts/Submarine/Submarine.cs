48a4b816633ec95846a77c588d415bd9d40538b5
AleksisBoy <aleboj9@gmail.com>
2024-09-03 11:43:40 +0200

    Added submarine script, depth meter

---
+++ LifeUnderPressure/Assets/_Scripts/Submarine/Submarine.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(SubmarineMovement))]
+public class Submarine : MonoBehaviour
+{
+    [SerializeField] private TMP_Text heightText = null;
+
+    private SubmarineMovement movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<SubmarineMovement>();
+    }
+    private void FixedUpdate()
+    {
+        float depth = -transform.position.y;
+        if (heightText) heightText.text = string.Format("Depth: {0:F1}", depth);
+    }
+}
