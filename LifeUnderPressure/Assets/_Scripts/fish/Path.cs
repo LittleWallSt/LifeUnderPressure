71bcd24c1aac043f6895f8c73ec0815882778a2b
Javier Murillo González <96345143+LittleWallSt@users.noreply.github.com>
2024-09-05 15:40:40 +0200

    Fish AI
    Simplified the fish AI removing unnecesary parts and adding scared behaviour and curious behaviour

---
+++ LifeUnderPressure/Assets/_Scripts/fish/Path.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 2f;
+
+    [SerializeField]
+    private Transform[] waypoints;
+
+    public int Length { get { return waypoints.Length; } }
+    public Transform GetWaypoint(int index) {  return waypoints[index]; }
+
+    public float Radius { get { return radius; } }
+
+    // Draws path in gizmos
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < waypoints.Length; i++) {
+            Debug.DrawLine(waypoints[i].position, waypoints[(i+1)%waypoints.Length].position, Color.magenta);
+        }
+    }
+}
