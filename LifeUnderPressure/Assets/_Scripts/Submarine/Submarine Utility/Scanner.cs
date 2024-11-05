e740aeb09c2985be0f25c1012296ee25d47f2f52
Javier Murillo González <96345143+LittleWallSt@users.noreply.github.com>
2024-11-05 12:02:37 +0100

    scanner fix

--- LifeUnderPressure/Assets/_Scripts/Submarine/Submarine Utility/Scanner.cs
+++ LifeUnderPressure/Assets/_Scripts/Submarine/Submarine Utility/Scanner.cs
@@ -196,12 +196,9 @@ public class Scanner : MonoBehaviour
     private void FinishedScanner()
     {
         // Aleksis >>
-        Fish fish = currentFish.transform.root.GetComponent<Fish>();
-        if (!fish) { Debug.LogError("No fish script on the object scanned"); return; }
-
-        FishInfo fishInfo = fish.FishInfo;
-        fishInfo.locked = false;
-        GameManager.Instance.ScannedFish(fishInfo);
+        Debug.Log(currentFish.transform.root);
+        FishInfo fishInfo = currentFish.GetComponent<Fish>().FishInfo;
+                fishInfo.locked = false;
         if (fishInfo.OnLockedChange != null) fishInfo.OnLockedChange.Invoke();
         QuestSystem.ScannedFish(fishInfo);
         DataManager.Write("FishScanned_" + fishInfo.fishName, 1);
