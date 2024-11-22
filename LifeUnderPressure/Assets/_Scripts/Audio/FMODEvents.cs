cdbb39a10c28988b92eb8b317df83d478c1653d9
Janko Zorbas <zorbasjanko@gmail.com>
2024-11-22 13:55:24 +0100

    Added cave collapse sfx to fmodevents

--- LifeUnderPressure/Assets/_Scripts/Audio/FMODEvents.cs
+++ LifeUnderPressure/Assets/_Scripts/Audio/FMODEvents.cs
@@ -24,6 +24,9 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField]
     public EventReference SFX_CookieCutter { get; set; }
 
+    [field: SerializeField]
+    public EventReference SFX_CaveCollapse { get; set; }
+
     [field: Header("Upgrades")]
     [field: SerializeField]
     public EventReference upgradeFX { get; private set; }
