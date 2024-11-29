17d7272d462e232940919526febb15ca6116319e
Sechkar Uliana <uliasia22@gmail.com>
2024-11-29 11:51:34 +0200

    rounded boost to 1 decimal

--- LifeUnderPressure/Assets/_Scripts/UI/Attributes/BoostUI.cs
+++ LifeUnderPressure/Assets/_Scripts/UI/Attributes/BoostUI.cs
@@ -10,6 +10,6 @@ public class BoostUI : MonoBehaviour
     public void UpdateUI(float value, float CD)
     {
         boostSlider.value = value;
-        CDText.text = "Boost CD: " + CD;
+        CDText.text = "Boost CD: " + Mathf.Round(CD * 10.0f) * 0.1f;
     }
 }
